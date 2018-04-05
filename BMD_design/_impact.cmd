a90069f06a236787924945be3266f285ca96831a
imjustadog <imjustadog@outlook.com>
2018-04-05 11:30:41 +0800

    add download

---
+++ BMD_design/_impact.cmd
@@ -0,0 +1,84 @@
+loadProjectFile -file "D:\GitHub\AE-PCIE-DMA\download\prom.ipf"
+setMode -bs
+setMode -ss
+setMode -sm
+setMode -hw140
+setMode -spi
+setMode -acecf
+setMode -acempm
+setMode -pff
+setMode -bs
+setMode -bs
+setMode -bs
+setMode -bs
+setCable -port auto
+setMode -pff
+setMode -pff
+addConfigDevice  -name "prom" -path "D:\GitHub\AE-PCIE-DMA\download\"
+setSubmode -pffspi
+setAttribute -configdevice -attr multibootBpiType -value ""
+addDesign -version 0 -name "0"
+setMode -pff
+addDeviceChain -index 0
+setMode -pff
+addDeviceChain -index 0
+setAttribute -configdevice -attr compressed -value "FALSE"
+setAttribute -configdevice -attr compressed -value "FALSE"
+setAttribute -configdevice -attr autoSize -value "FALSE"
+setAttribute -configdevice -attr fileFormat -value "mcs"
+setAttribute -configdevice -attr fillValue -value "FF"
+setAttribute -configdevice -attr swapBit -value "FALSE"
+setAttribute -configdevice -attr dir -value "UP"
+setAttribute -configdevice -attr multiboot -value "FALSE"
+setAttribute -configdevice -attr multiboot -value "FALSE"
+setAttribute -configdevice -attr spiSelected -value "TRUE"
+setAttribute -configdevice -attr spiSelected -value "TRUE"
+addPromDevice -p 1 -size 8192 -name 8M
+setMode -bs
+setMode -bs
+setMode -bs
+setMode -pff
+setMode -pff
+setMode -pff
+setMode -pff
+addDeviceChain -index 0
+setMode -pff
+addDeviceChain -index 0
+setSubmode -pffspi
+setMode -pff
+setAttribute -design -attr name -value "0000"
+addDevice -p 1 -file "D:/GitHub/AE-PCIE-DMA/BMD_design/xilinx_pcie_1_1_ep_s6.bit"
+setMode -pff
+setSubmode -pffspi
+generate
+setCurrentDesign -version 0
+setMode -bs
+setMode -bs
+setMode -bs
+setMode -bs
+setMode -bs
+setMode -ss
+setMode -sm
+setMode -hw140
+setMode -spi
+setMode -acecf
+setMode -acempm
+setMode -pff
+setMode -bs
+saveProjectFile -file "D:/GitHub/AE-PCIE-DMA/download/prom.ipf"
+setMode -bs
+setMode -pff
+setMode -bs
+setMode -bs
+setMode -ss
+setMode -sm
+setMode -hw140
+setMode -spi
+setMode -acecf
+setMode -acempm
+setMode -pff
+deletePromDevice -position 1
+setCurrentDesign -version 0
+deleteDevice -position 1
+deleteDesign -version 0
+setCurrentDesign -version -1
